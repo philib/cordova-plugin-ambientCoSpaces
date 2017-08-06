33d07af4b76c52783444df7be5285ef5cfc8b154
Philip Schaefer <00philip00@googlemail.com>
2017-08-06 19:13:17 +0200

    beacon logic added

--- src/android/AlgorithmService.java
+++ src/android/AlgorithmService.java
@@ -8,6 +8,34 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BleNotAvailableException;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Handler;
+import android.util.Log;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
@@ -17,9 +45,11 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
 import android.widget.Toast;
+import android.os.RemoteException;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Collection;
 
 public class AlgorithmService extends Service {
 
@@ -44,7 +74,7 @@ public class AlgorithmService extends Service {
         // start the service using the background handler
         mServiceHandler = new ServiceHandler(mServiceLooper);
 
-        backgroundThread = new BackgroundThread();
+        backgroundThread = new BackgroundThread(this);
     }
 
     @Override
@@ -88,9 +118,15 @@ public class AlgorithmService extends Service {
         }
     }
 
-    public class BackgroundThread extends Thread {
+    public class BackgroundThread extends Thread implements BeaconConsumer {
+
+        protected static final String TAG = "com.htwg.ambientcospaces";
+        private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
+        private BluetoothAdapter bluetoothAdapter;
+
         volatile boolean running = true;
         private Timer timer;
+        private Context context;
         private final Handler toastHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
@@ -98,8 +134,21 @@ public class AlgorithmService extends Service {
             }
         };
 
+        public BackgroundThread(Context context){
+            this.context = context;
+        }
+
+
         @Override
         public void run() {
+            try {
+                bluetoothAdapter.enable();
+                beaconManager.bind(this);
+            } catch (Exception e){
+                Log.e(TAG, "Thread run error : " + e.getCause());
+            }
+
+
             timer = new Timer();
             TimerTask myTask = new TimerTask() {
                 @Override
@@ -109,12 +158,49 @@ public class AlgorithmService extends Service {
             };
 
             timer.schedule(myTask, 2000, 2000);
-            while (!running) return;
+            while (!running){
+                beaconManager.unbind(this);
+                return;
+            }
+        }
+
+        @Override
+        public void onBeaconServiceConnect() {
+            beaconManager.setRangeNotifier(new RangeNotifier() {
+                @Override
+                public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                    if (beacons.size() > 0) {
+                        Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    }
+                }
+            });
+
+            try {
+                beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            } catch (RemoteException e) {    }
         }
 
         public void cancel() {
             timer.cancel();
             running = false;
+        }
+        //////// IBeaconConsumer implementation /////////////////////
+
+        @Override
+        public Context getApplicationContext() {
+            return this.context;
+        }
+
+        @Override
+        public void unbindService(ServiceConnection connection) {
+            Log.i(TAG,"Unbind from IBeacon service");
+            this.getApplicationContext().unbindService(connection);
+        }
+
+        @Override
+        public boolean bindService(Intent intent, ServiceConnection connection, int mode) {
+            Log.i(TAG,"Bind to IBeacon service");
+            return this.getApplicationContext().bindService(intent, connection, mode);
         }
     }
 }
