0e64f049d1909ffe93847ef2f1947260c77ed315
Philip Schaefer <philip.schaefer@htwg-konstanz.de>
2017-08-27 16:37:01 +0200

    service restarter added

---
+++ src/cordova/ambient/cospaces/positioning/algorithm/ServiceRestarter.java
@@ -0,0 +1,33 @@
+package ambient.cospaces.positioning.algorithm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class ServiceRestarter extends BroadcastReceiver {
+    protected static final String TAG = "com.htwg.ambientcospaces";
+    private boolean background;
+
+    public ServiceRestarter() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent in = new Intent(context, BackgroundService.class);
+        String action = intent.getAction();
+        Log.i(TAG, "BroadcastReciever recieved action = " + action);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        this.background = sharedPref.getBoolean("background", false);
+
+        if(this.background){
+            context.startService(in);
+            Log.i(TAG, "Restart Background Service After Boot");
+        }
+    }
+}
