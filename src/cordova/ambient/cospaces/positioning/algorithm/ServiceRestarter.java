59efcbb0bec93077b907c68108922c506aba4f34
Philip Schaefer <00philip00@googlemail.com>
2017-09-16 15:00:17 +0200

    listen on network changes

--- src/cordova/ambient/cospaces/positioning/algorithm/ServiceRestarter.java
+++ src/cordova/ambient/cospaces/positioning/algorithm/ServiceRestarter.java
@@ -4,8 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * Restarts BackgroundService after phone completed boooting process
@@ -28,9 +33,26 @@ public class ServiceRestarter extends BroadcastReceiver {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         this.background = sharedPref.getBoolean("background", false);
 
-        if(this.background){
-            context.startService(in);
-            Log.i(TAG, "Restart Background Service After Boot");
+        if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)){
+            Log.i(TAG, "Network State Changed");
+        }else if(action.equals(ConnectivityManager.CONNECTIVITY_ACTION)){
+            Log.i(TAG, "Connectivity Changed");
+        }else if(action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)){
+            WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+            List<ScanResult> scanResults = wifiManager.getScanResults();
+            Log.i(TAG, "ScanResults Available : "+scanResults.toString());
+            for(int i = 0; i < scanResults.size(); i++){
+                Log.i(TAG, "ScanResults SSID : "+scanResults.get(i).SSID);
+                if(scanResults.get(0).SSID.equals("eduroam")){
+                    context.startService(in);
+                    Log.i(TAG, "Restart Background Service After Eduroamfound");
+                }
+            }
+        }else if(action.equals(Intent.ACTION_BOOT_COMPLETED)){
+            if(this.background){
+                context.startService(in);
+                Log.i(TAG, "Restart Background Service After Boot");
+            }
         }
     }
 }
