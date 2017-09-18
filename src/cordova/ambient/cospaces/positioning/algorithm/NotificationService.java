d917b435aaa25ff8b9f3d784d0c329b09a0cca38
Philip Schaefer <00philip00@googlemail.com>
2017-09-18 13:31:36 +0200

    changed mqtt behavior

--- src/cordova/ambient/cospaces/positioning/algorithm/NotificationService.java
+++ src/cordova/ambient/cospaces/positioning/algorithm/NotificationService.java
@@ -12,7 +12,6 @@ import android.os.Looper;
 import android.os.Process;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.json.JSONObject;
@@ -23,7 +22,7 @@ public class NotificationService extends Service {
     private HandlerThread thread;
     private String accountId;
     protected static final String TAG = "com.htwg.ambientcospaces.notification";
-    private MqttAndroidClient client;
+    private MqttAsyncClient client;
 
     @Override
     public void onCreate() {
@@ -45,7 +44,7 @@ public class NotificationService extends Service {
             this.accountId = sharedPref.getString("accountId", null);
             Log.i(TAG, "AccountID: "+ this.accountId);
             //JSONObject user = new JSONObject(userString);
-            this.subscribe();
+            this.connect();
         } catch (Exception e) {
             Log.i(TAG, "Error: "+ e.getMessage());
             e.printStackTrace();
@@ -67,72 +66,125 @@ public class NotificationService extends Service {
     }
 
     private void subscribe(){
-        this.client = new MqttAndroidClient(this.getApplicationContext(),
-                "tcp://acs.in.htwg-konstanz.de:1883", //URI
-                this.accountId,
-                new MemoryPersistence()); //ClientId
+//        this.client = new MqttAndroidClient(this.getApplicationContext(),
+//                "tcp://acs.in.htwg-konstanz.de:1883", //URI
+//                this.accountId,
+//                new MemoryPersistence()); //ClientId
+//        try {
+//            client.setCallback(new MqttCallback() {
+//
+//                @Override
+//                public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
+//                    Log.i(TAG, "Connection Lost!");
+//                }
+//
+//                @Override
+//                public void messageArrived(String topic, MqttMessage message) throws Exception {
+//                    Log.i(TAG, "Message arrived!");
+//                    JSONObject messageJSON = new JSONObject(message.getPayload().toString());
+//
+//                    Notification n = new Notification.Builder(getApplicationContext())
+//                            .setContentTitle(messageJSON.getString("title"))
+//                            .setContentText(messageJSON.getString("message")).build();
+//
+//                    NotificationManager notificationManager =
+//                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//
+//                    notificationManager.notify(0, n);
+//                }
+//
+//                @Override
+//                public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
+//                    Log.i(TAG, "Connection Lost!");
+//                }
+//            });
+//
+//            MqttConnectOptions options = new MqttConnectOptions();
+//            options.setUserName("guest");
+//            options.setPassword("guest".toCharArray());
+//
+//            Log.i(TAG, "Client connect!");
+//            IMqttToken iMqttToken = this.client.connect(options);
+//            iMqttToken.setActionCallback(new IMqttActionListener() {
+//                @Override
+//                public void onSuccess(IMqttToken iMqttToken) {
+//                    try {
+//                        client.subscribe(accountId, 1);
+//                    } catch (MqttException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                @Override
+//                public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+//
+//                }
+//            });
+//            Log.i(TAG, "Subscribe:" + this.client + " " + this.accountId + " " + options);
+//        } catch (Exception e) {
+//            Log.i(TAG, "Subscribe Error :" + e.getMessage());
+//            e.printStackTrace();
+//        }
+    }
+
+    private void unsubscribe() {
+//        try {
+//            Log.i(TAG, "Unsubscribe and disconnect");
+//            this.client.unsubscribe(this.accountId);
+//            this.client.disconnect();
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private void connect(){
+        MemoryPersistence persistence = new MemoryPersistence();
         try {
-            client.setCallback(new MqttCallback() {
+            client = new MqttAsyncClient("tcp://acs.in.htwg-konstanz.de:1883", accountId, persistence);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
 
-                @Override
-                public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
-                    Log.i(TAG, "Connection Lost!");
-                }
+        final MqttConnectOptions connOpts = new MqttConnectOptions();
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.i(TAG, "Connection Lost " + cause.toString());
+            }
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG, "topic is " + topic + ". payload is " + message.toString());
+            }
 
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Log.i(TAG, "Message arrived!");
-                    JSONObject messageJSON = new JSONObject(message.getPayload().toString());
-
-                    Notification n = new Notification.Builder(getApplicationContext())
-                            .setContentTitle(messageJSON.getString("title"))
-                            .setContentText(messageJSON.getString("message")).build();
-
-                    NotificationManager notificationManager =
-                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-                    notificationManager.notify(0, n);
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
-                    Log.i(TAG, "Connection Lost!");
-                }
-            });
-
-            MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName("guest");
-            options.setPassword("guest".toCharArray());
-
-            Log.i(TAG, "Client connect!");
-            IMqttToken iMqttToken = this.client.connect(options);
-            iMqttToken.setActionCallback(new IMqttActionListener() {
-                @Override
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.i(TAG, "Delivery complete ");
+            }
+        });
+        try {
+            client.connect(connOpts, null, new IMqttActionListener() {
                 public void onSuccess(IMqttToken iMqttToken) {
+                    Log.i(TAG, "Connected");
                     try {
-                        client.subscribe(accountId, 1);
+                        Log.i(TAG, "Subscribe");
+                        client.subscribe(accountId, 1, null, new IMqttActionListener() {
+                            public void onSuccess(IMqttToken iMqttToken) {
+                                Log.i(TAG, "Subscribe successfull");
+                            }
+
+                            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                                Log.i(TAG, "Subscribe failed");
+                            }
+                        });
                     } catch (MqttException e) {
                         e.printStackTrace();
                     }
                 }
 
-                @Override
                 public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
-
+                    Log.i(TAG, "Connect failed");
                 }
             });
-            Log.i(TAG, "Subscribe:" + this.client + " " + this.accountId + " " + options);
-        } catch (Exception e) {
-            Log.i(TAG, "Subscribe Error :" + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private void unsubscribe() {
-        try {
-            Log.i(TAG, "Unsubscribe and disconnect");
-            this.client.unsubscribe(this.accountId);
-            this.client.disconnect();
         } catch (MqttException e) {
             e.printStackTrace();
         }
